b57cfd362c6295bf365308e6582345190acb365c
Elliot Shiu <elliot@sandbochs.com>
2014-03-14 03:01:46 -0700

    Remove unused constants in BicycleParking

--- app/models/bicycle_parking.rb
+++ app/models/bicycle_parking.rb
@@ -1,21 +1,19 @@
 class BicycleParking < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   scope :completed, -> { where status: 'COMPLETE' }
-  MIN_RESULTS = 3
-  START_DIAMETER = 0.5
-  MAX_DIAMETER = 5
-  SEARCH_PARAMS = { min_results: 3, start_diameter: 0.5, max_diameter: 0.5 }
+
+  SEARCH_PARAMS = { min_results: 3, start_diameter: 0.5, max_diameter: 5 }
 
   def self.search_within(lat, long, min_results=1, results=[],  diameter=0.1, inc_diameter=0.1, max_diameter=20)
     return results if results.length >= min_results || diameter > max_diameter
 
     # Consider the runtime complexity of this recursive call. As the diameter increases we rescan the previous search area.
     nearby_parking = self.completed.near([lat, long], diameter)
-    search_within lat, long, min_results, nearby_parking, diameter + inc_diameter, max_diameter
+    search_within lat, long, min_results, nearby_parking, diameter + inc_diameter, inc_diameter, max_diameter
   end
 
   def self.search(lat, long, max_results)
-    nearby_parking = search_within lat, long, MIN_RESULTS, [], START_DIAMETER, START_DIAMETER, MAX_DIAMETER
+    nearby_parking = search_within lat, long, SEARCH_PARAMS[:min_results], [], SEARCH_PARAMS[:start_diameter], SEARCH_PARAMS[:start_diameter], SEARCH_PARAMS[:max_diameter]
     nearby_parking.length > max_results ? nearby_parking[0...max_results] : nearby_parking
   end
 end
